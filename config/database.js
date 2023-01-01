691bd10be554234c75126a44d9ee9a2ab0ed20bc
Chiranjeev-droid <somu209e@gmail.com>
2023-01-01 21:55:27 +0530

    minor bug fixed

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+//connecting Mongoose database
 export const connectDB = async (req, res) => {
   const { connection } = await mongoose.connect(process.env.MONGO_URI);
   console.log(`Database is connected with ${connection.host}`);
